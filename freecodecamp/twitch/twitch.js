cd35f50138311820f9dc584db177bc6cdafb283f
Mirakurun <chhaykevin@gmail.com>
2017-03-18 23:29:02 -0700

    add twitch.js and update index.html

---
+++ freecodecamp/twitch/twitch.js
@@ -0,0 +1,20 @@
+function getChannels {
+    var channels = ["dansgaming"];
+    var url = "https://api.twitch.tv/kraken/streams?callback=?&client_id=o0azfla1ko5uwng0vr8mk40nj4xapx&channel=" + channels;
+
+    $.ajax({
+        url: url,
+        type: "GET",
+    }).done(function(data){
+        console.log(data);
+    })
+
+
+
+
+}
+
+$(document).ready(function(){
+    getChannels();
+
+});
