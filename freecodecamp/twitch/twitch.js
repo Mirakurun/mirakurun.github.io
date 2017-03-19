20107701bcf9a5a301300c7a212bc1bbb43d154b
Mirakurun <chhaykevin@gmail.com>
2017-03-19 01:13:32 -0700

    update

--- freecodecamp/twitch/twitch.js
+++ freecodecamp/twitch/twitch.js
@@ -3,7 +3,7 @@ function getChannels() {
     var url = "https://api.twitch.tv/kraken/streams?callback=?&client_id=o0azfla1ko5uwng0vr8mk40nj4xapx&channel=" + channels;
 
     $.ajax({
-        url: url,
+        url: "https://api.twitch.tv/kraken/streams/dansgaming?client_id=o0azfla1ko5uwng0vr8mk40nj4xapx",
         type: "GET",
     }).done(function(data){
         console.log("hello world");
