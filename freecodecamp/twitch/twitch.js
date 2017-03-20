fd49ccba488c143ddc7ea4faaaf9c8e66a226cdf
Mirakurun <chhaykevin@gmail.com>
2017-03-20 14:27:29 -0700

    update

--- freecodecamp/twitch/twitch.js
+++ freecodecamp/twitch/twitch.js
@@ -1,8 +1,9 @@
 function getChannels() {
     var channels = ["TwitchPresents", "dansgaming", "admiralbahroo", "maximilian_dood", "annemunition"];
-    var url = "https://api.twitch.tv/kraken/streams/" + val + "&client_id=o0azfla1ko5uwng0vr8mk40nj4xapx";
 
     channels.forEach(function(val){
+        var url = "https://api.twitch.tv/kraken/streams/" + val + "&client_id=o0azfla1ko5uwng0vr8mk40nj4xapx";
+
         $.ajax({
             url: url,
             type: "GET",
